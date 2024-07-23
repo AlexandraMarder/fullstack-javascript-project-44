5955eea4b3d1b1b84dc5d5a294b716ecd921bb21
Alexandra Marder <aleksandra.matcuka@gmail.com>
2024-07-23 23:45:04 +0200

    fix linter errors

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -7,13 +7,13 @@ const getTitle = () => {
 const findDivider = (a, b) => {
   let num1 = a;
   let num2 = b;
-  
+
   while (num2 !== 0) {
     const temp = num2;
     num2 = num1 % num2;
     num1 = temp;
   }
-  
+
   return num1;
 };
 
